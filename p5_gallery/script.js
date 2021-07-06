51db9939431bc1be20b60fc8896025102ac39d81
Artur <vashpartner@gmail.com>
2021-07-06 22:23:14 +0300

    Добавление точек

--- p5_gallery/script.js
+++ p5_gallery/script.js
@@ -1,9 +1,11 @@
 //--------------------------TapGallery----
 
 const glrImg =[
-    {link:'./img/g4haiwkwu7jqrhh65c804phjt-q.jpeg' },
-    {link:'./img/mothman-frank-frazetta.jpg' },
-    {link:'./img/The-Art-of-Frank-Frazetta-9.jpeg' }
+    {link:'./img/g4haiwkwu7jqrhh65c804phjt-q.jpeg', desc:"Image 1" },
+    {link:'./img/mothman-frank-frazetta.jpg', desc:"Motman by Frank Frazetta" },
+    {link:'./img/The-Art-of-Frank-Frazetta-9.jpeg', desc:"Image 3" },
+    {link:'./img/StudioLightTest_21.jpg', desc:"Red sphere" }
+    
   ]
   let curentStep = 0
   
@@ -93,206 +95,10 @@ const glrImg =[
       allDots[curentStep].style.transform = "scale(1.5)"
     } // Берёт все точки и обновляет цвет и размер в соответствии с текущим номером изображения
 
-
-//-----------------------------Listener---DragbleCube-----------------------------------------------------------------------------------------------//
-
-const lis = document.querySelector('.listener__block')
-let triger = false
-let offset = 0
-let curentStepTouch = 0
-// const moveBox = document.querySelector('.listener__box')
-initBoxes('.listener__block',3)
-renderImg('.listener__box',1)
+//------------------------------------------------------------------------------------------------------------------------------//
 
 
-lis.addEventListener("mousemove",(e)=>{
-  // console.log('Down')
-  document.querySelector('.listener_info').innerHTML =`<div>X: ${e.x}  Y:${e.y}</div>
-  <div>X: ${e.layerX}  Y:${e.layerY}</div>
-  <div>X: ${e.movementX}  Y:${e.movementY}</div>`
-  // lis.addEventListener("mousemove",(e)=>{
-  //   console.log('Moviing')})
- 
-   
-  if (triger && e.x >= 200 && e.x <=500){
-    console.log(triger)
-    console.log(offset)
-     lis.style.left = `${e.x - offset}px`
-   }
-  
-})
-
-// let x = 10;
-
-lis.addEventListener("mousedown",(e)=>{
-  lis.style.background="red"
-  triger = true
-  // console.log(e)
-  offset = e.layerX 
-  document.body.style.overflow = "hidden"
-})
-
-lis.addEventListener("mouseup",(e)=>{
-  lis.style.background="rgba(100, 148, 237, 0.843)"
-  triger = false  
-  document.body.style.overflow = ""
-  // moveBox.style.transition =""
-
-  allBoxesStyle('.listener__box',true)
-})
-
-lis.addEventListener("mouseout",(e)=>{
-  lis.style.background="rgba(100, 148, 237, 0.843)"
-  triger = false  
-})
-//-------------------Touch events
-let startX = 0, endX = 0
-
-lis.addEventListener("touchmove",(e)=>{
-  // console.log('Down')
-  document.querySelector('.listener_info').innerHTML =`<div>X: ${e.x}  Y:${e.y}</div>
-  <div>clientX: ${Math.floor(e.touches[0].clientX)}  Y:${Math.floor(e.touches[0].clientY)}</div>
-  <div>screenX: ${Math.floor(e.touches[0].screenX)}  Y:${Math.floor(e.touches[0].screenY)}</div>
-  <div>pageX: ${Math.floor(e.touches[0].pageX)}  pageY:${Math.floor(e.touches[0].pageY)}</div>
-  <div>X: ${window.getComputedStyle(lis).width}  Y:${window.getComputedStyle(lis).height}</div>
-  <div>radX: ${Math.floor(e.touches[0].radiusX)}  radY:${Math.floor(e.touches[0].radiusY)}</div>`
-  console.log('Touch Moviing')
-  // все что выше, сервиная информация
-
-  let curentPosX = Math.floor(e.touches[0].clientX)
-  moveAllBoxes(curentPosX, startX)
-  // moveBox.style.transform =`translate(${()-30}px, 170px)`
- 
- 
-  //двигает большой куб от событий мыши
-  if (triger && e.x >= 200 && e.x <=500){
-    console.log(triger)
-    console.log(offset)
-     lis.style.left = `${e.x - offset}px`
-   }
-  
-})
-
-lis.addEventListener("touchstart",(e)=>{
-  lis.style.background="red"
-  triger = true
-  // console.log(e)
-
-  allBoxesStyle('.listener__box',false)
-  console.log('Touch Start')
-  // 
-  startX = Math.floor(e.touches[0].clientX)
-  document.body.style.overflow = "hidden"
-  // moveBox.style.transition =""
-
-})
-
-lis.addEventListener("touchend",(e)=>{
-  lis.style.background="rgba(100, 148, 237, 0.843)"
-  triger = false 
-  // console.log(e.changedTouches[0].clientX) 
-  endX = Math.floor(e.changedTouches[0].clientX)
-  swipeInfo()
-  
-
-
-  document.body.style.overflow = ""
-  // moveBox.style.transition ="all 0.3s"
-  // moveBox.style.transform =`translate(150px, 170px)` //start position
-})
-
-function swipeInfo(){
-  let state
-if(Math.abs(startX-endX) >= 80){
-  if(startX > endX){
-    state = "<<<<"
-    curentStepTouch --
-    moveAllBoxes(30,0)
-    allBoxesStyle('.listener__box',true)
-
-  } else {
-    state = ">>>>"
-    
-    moveForward(50)
-    allBoxesStyle('.listener__box',true)
-        
-  }
-} else { 
-  state = "Stay"
-  moveAllBoxes(0,0)
-  allBoxesStyle('.listener__box',true)
-}
- 
-  document.querySelector('.listener_info').innerHTML +=`<div>Direction: ${state} ${Math.abs(startX-endX)} </div>`
-  
-}
-////----------------------------------new iter
-
-function initBoxes(target,amount){
-  for(let i=0; i < amount; i++){
-    document.querySelector(target).innerHTML += `<div class="listener__box"></div>`
-  } 
-  const allBoxes = document.querySelectorAll('.listener__box')
-  let offsetPosX = 0
-  allBoxes.forEach(item=>{
-    item.style.transform=`translate(${offsetPosX}px, 150px)`
-    offsetPosX += 20
-  })
-} //отрисовывает указанное количсетво боксов, с заданным интервалом по Х
-
-function moveAllBoxes(positionX,startTouch){
-  const allBoxes = document.querySelectorAll('.listener__box')
-  let offsetPosX = 0
-  allBoxes.forEach(item=>{
-    item.style.transform =`translate(${positionX-20 + offsetPosX - startTouch}px, 150px)`
-    offsetPosX += 20
-  }) 
-} // двигает все боксы относительно их начала кооринат
-
-function allBoxesStyle(target,state){
-  if(state){
-    document.querySelectorAll(target).forEach(item=>{
-      item.style.transition ="all 0.3s"
-    })
-  } else {
-    document.querySelectorAll(target).forEach(item=>{
-      item.style.transition =""
-    })
-  }
-} //добавляет и убирает свойство транзишн
-
-function renderImg(target,index){
-  document.querySelectorAll(target).forEach(item=>{item.style.background="yellow"})
-  document.querySelectorAll(target)[index].style.background="violet"
-  
-}
-
-function moveForward(gap){
-  moveAllBoxes(gap,0)
-  addBox('.listener__block')
-  renderImg('.listener__box',1)
-  removeBox('.listener__block')
-
-  // document.querySelectorAll(target).forEach(item=>{
-
-  // })
-}
-
-function addBox(target) {
-  document.querySelector(target).insertAdjacentHTML('afterbegin', `<div class="listener__box"></div>`)
-  document.querySelectorAll('.listener__box')[0].style.transform = "translate(0px, 150px)"
-}  //добавляет один блок
-
-function removeBox(target){
-  // const lChild = document.querySelector(target).lastChild
-  const lChild = document.querySelectorAll('.listener__box')[2]
-  console.log(lChild)
-  // lChild.style.background="green"
-  document.querySelector(target).removeChild(lChild)
-
-}
-
-//---------------------------------------------
+//------------------------------------------------------------------------------------------------------------ Gallery class -----------------------
 
 class TouchGallery {
   constructor(target='body', imgArray = ['Hello'], width=300,height=300){
@@ -310,10 +116,10 @@ class TouchGallery {
 
     this.imgContent = document.querySelector(`${this.target} .glrT__imageContent`) 
     this.imgContent.addEventListener("touchmove",(e)=>{
-      console.log('Mooooove')
+      // console.log('Mooooove')
       
       this.curentPosX = Math.floor(e.touches[0].clientX)
-      console.log(this.startX)
+      // console.log(this.startX)
       this.moveAllBoxes(this.curentPosX,this.startX)
     })
 
@@ -326,7 +132,6 @@ class TouchGallery {
 
     this.imgContent.addEventListener("touchend",(e)=>{
       this.endX = Math.floor(e.changedTouches[0].clientX)
-      // this.moveAllBoxes(0,0)
       this.allBoxesStyle(true)
       this.swipeInfo()
       document.body.style.overflow = ""
@@ -334,7 +139,6 @@ class TouchGallery {
     })
     this.imgContent.addEventListener("mouseup",(e)=>{
       this.endX = Math.floor(e.changedTouches[0].clientX)
-      // this.moveAllBoxes(0,0)
       this.allBoxesStyle(true)
       this.swipeInfo()
       document.body.style.overflow = ""
@@ -355,7 +159,7 @@ class TouchGallery {
             </div>          
       </div>
     </div>`
-    this.glrTCounter()
+    this.glrTCheckLength()
   }
 
   renderImg(){
@@ -363,9 +167,11 @@ class TouchGallery {
 
     for(let i=0; i<3; i++){
         this.glrTForward()
-        document.querySelector(`${this.target} .glrT__imageContent`).innerHTML+=`<div style="background:url('${this.iArray[this.curentStep].link}') center/cover no-repeat; width:${this.width}px; height:${this.height}px; transform:translateX(${offsetX}px)" class="glrT__imageItem">Image</div>`
+        document.querySelector(`${this.target} .glrT__imageContent`).innerHTML+=`<div style="background:url('${this.iArray[this.curentStep].link}') center/cover no-repeat; width:${this.width}px; height:${this.height}px; transform:translateX(${offsetX}px)" class="glrT__imageItem"></div>`
         offsetX += this.width
     }
+    this.glrTCheckUpdate()
+    this.glrTDescUpdate()
 
 
 
@@ -401,21 +207,23 @@ class TouchGallery {
   if(Math.abs(this.startX-this.endX) >= 80){
       if(this.startX > this.endX){
         console.log("<<<<")
-        this.moveAllBoxes(-300,0)
+        this.moveAllBoxes(-this.width,0)
         this.removeBox(true)
         this.addBox(true)
         this.glrTForward()
-        this.glrTCounter()
+        this.glrTCheckUpdate()
+        this.glrTDescUpdate()
         
         
         
       } else {
         console.log(">>>>")
-        this.moveAllBoxes(300,0)
+        this.moveAllBoxes(this.width,0)
         this.removeBox(false)
         this.addBox(false) 
         this.glrTBackward()
-        this.glrTCounter()
+        this.glrTCheckUpdate()
+        this.glrTDescUpdate()
        
   
       }
@@ -438,9 +246,7 @@ class TouchGallery {
     }
   } //добавляет и убирает свойство транзишн
   
-  glrTCounter(){
-    document.querySelector(`${this.target} .glrT__counter`).innerHTML =`<div class="glrT__counterContent"> ${this.curentStep+1}/${this.lenghtArr} </div>`
-  } //Обрабатывае значение счетчика
+  
 
   removeBox(first){
     // const lChild = document.querySelector(target).lastChild
@@ -490,6 +296,50 @@ class TouchGallery {
     
   } //Обрабатывае событие назад
 
+  //------------------------------------------------------------------------Counter
+
+  glrTCheckLength(){
+    document.querySelector(`${this.target} .glrT__counter`).innerHTML ='' //очистка содержимого
+    if(this.lenghtArr <= 5){
+      for(let i=0; i < this.lenghtArr; i++){
+        document.querySelector(`${this.target} .glrT__counter`).innerHTML +=`<div class="gallery__dots"></div>`
+  
+      }
+    } else {
+        this.glrTCounter()
+      }
+  
+    } // Проверяет длинну массива изображений, если меньше 5 отрисовывает точки, если больше цифровой счетчик
+  
+  glrTCheckUpdate(){
+      if(this.lenghtArr <= 5){
+        this.glrTDotsUpdate()
+      } else {
+        this.glrTCounter()
+    }
+  }  //Проверяет длинну массива изображений в зависимости от нее вызывает апдейт Счетчика или Дотера
+  
+  glrTDotsUpdate() {
+      // console.log('DotsUpdate')
+      const allDots = document.querySelectorAll(`${this.target} .gallery__dots`)
+      allDots.forEach((dot)=>{
+        dot.style.background = "grey"
+        dot.style.transform = "scale(1)"
+      
+      }) // Сбрасывает стили у всех точек
+      
+      allDots[this.curentStep].style.background = "red"
+      allDots[this.curentStep].style.transform = "scale(1.5)"
+    } // Берёт все точки и обновляет цвет и размер в соответствии с текущим номером изображения
+
+  glrTCounter(){
+    document.querySelector(`${this.target} .glrT__counter`).innerHTML =`<div class="glrT__counterContent"> ${this.curentStep+1}/${this.lenghtArr} </div>`
+  } //Обрабатывае значение счетчика
+
+  glrTDescUpdate(){
+    document.querySelector(`${this.target} .glrT__desc`).innerHTML =`<div class="glrT__descText"> ${this.iArray[this.curentStep].desc} </div>`
+  } //Обновляет значение Description
+
 
 
 }
@@ -498,4 +348,4 @@ const a = new TouchGallery('.glwrap', glrImg);
 const b = new TouchGallery('.glwrap2', glrImg);
 
 
-  
+//---------------------------------------------------------TestInfo
