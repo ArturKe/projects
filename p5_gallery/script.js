3364228e197add4fb09ed79b6c8562828a0cfe65
Artur <vashpartner@gmail.com>
2021-07-16 11:42:46 +0300

    Добавления Скейла

--- p5_gallery/script.js
+++ p5_gallery/script.js
@@ -418,11 +418,415 @@ class TouchGallery {
 
 }
 
+//--------------------------------------------------------------------------- New Class ------------------------------------------------------------------------------//
+class TouchGalleryScale {
+  constructor(target='body', imgArray = ['Hello'], width=300,height=300){
+    this.iArray = imgArray;
+    this.lenghtArr = this.iArray.length;
+    this.width = width;
+    this.height = height;
+    this.target = target;
+    this.startX = 0;
+    this.endX = 0;
+    this.curentStep = 0;
+    this.touchStart = false;
+    this.multiTouch = false;
+    let isTouched = false
+    let isMultitouched = false
+    let vectorDistance = 0,
+      startVectorDistance = 0,
+      remapedDistance = 0
+      let rem = createRemap(50,300,0,2)
+      let zamok = false
+
+    this.init();
+    this.renderImg();
+    this.eventBind();
+
+  }
+
+  init(){
+    document.querySelector(this.target).innerHTML+=`
+    <div class="glrT">
+        <div class="glrT__body">  
+            <div class="glrT__content">
+                <div class="glrT__imageContent"></div>
+                <div class="glrT__counter">Counter</div>
+                <div class="glrT__desc">Descripstion</div>
+            </div>          
+      </div>
+    </div>` 
+    document.querySelector(`${this.target} .glrT__imageContent`).style.width =`${this.width}px`
+    document.querySelector(`${this.target} .glrT__imageContent`).style.height =`${this.height}px`
+    document.querySelector(`${this.target} .glrT__desc`).style.width =`${this.width}px`
+    this.glrTCheckLength()
+  } // Отрисовка шаблона
+
+  renderImg(){
+    let offsetX = parseInt(`-${this.width}`)
+    this.glrTBackward()
+
+    for(let i=0; i<3; i++){
+        document.querySelector(`${this.target} .glrT__imageContent`).innerHTML+=`<div style="background:url('${this.iArray[this.curentStep].link}') center/cover no-repeat; width:${this.width}px; height:${this.height}px; transform:translateX(${offsetX}px)" class="glrT__imageItem"></div>`
+        offsetX += this.width
+        this.glrTForward()
+    }
+    this.glrTBackward()
+    this.glrTBackward()
+    this.glrTCheckUpdate()
+    this.glrTDescUpdate()
+
+  } //Создает 3 блока в шаблоне и выводит в них первые 3 изображения из массива 
+  
+  //------------------------------------------------------------------------------------------------------------------Bind
+  eventBind(){
+    this.imgContent = document.querySelector(`${this.target} .glrT__imageContent`) 
+
+
+    this.imgContent.addEventListener("touchmove",(e)=>{
+      // e.preventDefault()
+      // e.stopPropagation()
+    this.updateTouchState(e)
+    
+     
+     
+      // if(e.touches.length >= 2){
+      //   this.curentPosXtouch2 = Math.floor(e.touches[1].clientX)
+      //   this.scalePicture(this.curentPosX,this.curentPosXtouch2)
+      // } else {
+      //   this.moveAllBoxes(this.curentPosX,this.startX)
+      // } ------------------------------------------------------------------------------Scale
+
+
+      
+    })
+    
+    this.imgContent.addEventListener("touchstart",(e)=>{
+      this.updateTouchState(e)
+
+
+      this.startX = Math.floor(e.touches[0].clientX)
+      document.body.style.overflow = "hidden"
+      this.allBoxesStyle(false)
+      this.touchStart = true
+      if(e.touches.length >= 2){
+        this.multiTouch = true
+      }
+
+    })
+
+    this.imgContent.addEventListener("touchend",(e)=>{
+      this.updateTouchState(e)
+
+
+      this.endX = Math.floor(e.changedTouches[0].clientX)
+      this.allBoxesStyle(true)
+      this.swipeInfo()
+      document.body.style.overflow = ""
+      this.touchStart = false
+      this.multiTouch = false
+      this.scalePictureReset()
+
+    })
+
+    //--------------------------------------------------------------- Mouse Up
+    this.imgContent.addEventListener("mouseup",(e)=>{
+      let MouseEndX = Math.floor(e.layerX)
+
+      if((MouseEndX <= this.width/2) && !this.touchStart){
+        this.actionMoveForward()
+      } 
+      
+      if((MouseEndX >= this.width/2) && !this.touchStart){
+        this.actionMoveBackward()
+      }
+
+      this.allBoxesStyle(true)
+      this.swipeInfo()
+      document.body.style.overflow = ""
+
+    })
+
+  } // Добавляет слушатели на разные элементы
+
+  updateTouchState(e){
+    // console.log(e)
+    e.stopPropagation()
+    e.preventDefault()
+    console.log(e.type)
+
+    try{
+      if(e.type === 'touchmove'){
+        this.curentPosX = Math.floor(e.touches[0].clientX)
+        this.moveAllBoxes(this.curentPosX,this.startX)
+      }
+      
+
+    }
+    catch{console.log('No Value X')}
+    
+  
+    if(e.touches.length > 0){
+      isTouched = true
+    } else {isTouched = false}
+
+    // console.log(isTouched)
+  
+    if(e.touches.length >= 2){
+      isMultitouched = true
+    } else {
+      isMultitouched = false
+    }
+  
+    if(isMultitouched){
+      if(!zamok){
+        startVectorDistance =e.touches[0].clientX - e.touches[1].clientX
+        zamok = true
+      }
+    } else {
+      zamok = false
+      startVectorDistance = 0
+    } 
+  
+  
+    if(isMultitouched){
+      let a = e.touches[0].clientX - e.touches[1].clientX
+      let b = e.touches[0].clientY - e.touches[1].clientY
+      vectorDistance = Math.sqrt(a*a+b*b)
+      remapedDistance = (rem(vectorDistance))
+      this.scalePicture(remapedDistance)
+    } else {
+      vectorDistance = 0
+      remapedDistance = 0
+      // this.scalePicture(1)
+    }
+    
+    // if(isMultitouched){
+    //   remapedDistance = (rem(vectorDistance))
+    // } else {
+    //   remapedDistance = 0
+    // }
+  
+    // if(isMultitouched){
+    //   for(let i=0; i < e.touches.length; i++){
+    //     this.imgContent.innerHTML+= `<div>Touch:${i} ClientX: ${Math.floor(e.touches[i].clientX)} ClientY: ${Math.floor(e.touches[i].clientY)}</div>`
+    //   }
+    //   // e.touches.forEach((item) => {
+    //   //   testBox.innerHTML+= `<div>Touch:${index} ClientX: ${Math.floor(item.clientX)}</div>`
+    //   // })
+    // } else {
+    //   try{this.imgContent.innerHTML+= `<div>CleintX: ${Math.floor(e.touches[0].clientX)} CleintY: ${Math.floor(e.touches[0].clientY)}</div>`}
+    //   catch{console.log('oh oh')}
+  
+    // }
+  
+  }
+
+
+  createRemap(inMin, inMax, outMin, outMax) {
+    return function remaper(x) {
+        return (x - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+    };
+  }
+  
+  setTouchState(value){
+    if (value){
+      touchStart = true; 
+      
+    } else {
+      touchStart = false;
+     
+    }
+  }
+
+
+
+  //------------------------------------------------------------------------------------------------------------------Bind
+
+  swipeInfo(){
+    let state
+    if(Math.abs(this.startX-this.endX) >= 80){
+        if(this.startX > this.endX){
+          this.actionMoveBackward()
+          
+        } else {
+          this.actionMoveForward()
+        }
+      } else { 
+        console.log("Stay")
+        this.moveAllBoxes(0,0)
+        }
+    } //----------------------------------------------------------------------------------------------------------------Swipe
+
+
+  scalePicture(scale){
+    // document.querySelectorAll(`${this.target} .glrT__imageItem`)[1].style.background="red"
+    // let scale = remap((x2-x1),30,250,1,3)
+    document.querySelectorAll(`${this.target} .glrT__imageItem`)[1].style.transform=`scale(${scale})`
+    
+
+  }
+  scalePictureReset(){
+    document.querySelectorAll(`${this.target} .glrT__imageItem`)[1].style.transform=`scale(1)`
+    // document.querySelectorAll(`${this.target} .glrT__imageItem`)[1].style.background="blue"
+
+  }
+
+  
+
+  moveAllBoxes(positionX,startTouch){
+    const allImg = document.querySelectorAll(`${this.target} .glrT__imageItem`)
+    let offsetPosX = 0
+    allImg.forEach(item=>{
+      item.style.transform =`translate(${positionX - this.width + offsetPosX - startTouch}px, 0px)`
+      offsetPosX += this.width
+    }) 
+  } // двигает все боксы относительно их начала кооринат
+
+  
+
+  actionMoveBackward(){
+    console.log("<<<<")
+        this.moveAllBoxes(-this.width,0)
+        this.glrTForward()
+
+        this.removeBox(true)
+        this.addBox(true)
+        
+        this.glrTCheckUpdate()
+        this.glrTDescUpdate()
+  }
+
+  actionMoveForward(){
+    console.log(">>>>")
+    this.moveAllBoxes(this.width,0)
+    this.glrTBackward()
+
+    this.removeBox(false)
+    this.addBox(false) 
+    
+    this.glrTCheckUpdate()
+    this.glrTDescUpdate()
+
+  }
+
+  allBoxesStyle(state){
+    if(state){
+      document.querySelectorAll(`${this.target} .glrT__imageItem`).forEach(item=>{
+        item.style.transition ="all 0.3s"
+      })
+    } else {
+      document.querySelectorAll(`${this.target} .glrT__imageItem`).forEach(item=>{
+        item.style.transition =""
+      })
+    }
+  } //добавляет и убирает свойство транзишн
+  
+  
+
+  removeBox(first){
+    let lChild 
+    if(first){
+      lChild= document.querySelectorAll(`${this.target} .glrT__imageItem`)[0]
+    } else {
+      lChild= document.querySelectorAll(`${this.target} .glrT__imageItem`)[2]
+    }
+    // console.log(lChild)
+    // lChild.style.background="green"
+    document.querySelector(`${this.target} .glrT__imageContent`).removeChild(lChild)
+  } //Удаляет первый или последний элемент
+
+  addBox(first){
+    if(first){
+      this.glrTForward()
+      document.querySelector(`${this.target} .glrT__imageContent`).insertAdjacentHTML('beforeend', `<div style="background:url('${this.iArray[this.curentStep].link}') center/cover no-repeat; width:${this.width}px; height:${this.height}px" class="glrT__imageItem"></div>`)
+      document.querySelectorAll(`${this.target} .glrT__imageItem`)[2].style.transform = `translate(${this.width}px, 0px)`
+      this.glrTBackward()
+    
+      
+    } else {
+      this.glrTBackward()
+      document.querySelector(`${this.target} .glrT__imageContent`).insertAdjacentHTML('afterbegin', `<div style="background:url('${this.iArray[this.curentStep].link}') center/cover no-repeat; width:${this.width}px; height:${this.height}px" class="glrT__imageItem"></div>`)
+      document.querySelectorAll(`${this.target} .glrT__imageItem`)[0].style.transform = `translate(${-this.width}px, 0px)`
+      this.glrTForward()
+    }
+
+  }  //добавляет один блок в начало или конец списка в зависимости условия
+
+  //--------------------------------------------------------------------------------------
+
+  glrTForward(){
+    if (this.curentStep >= (this.lenghtArr-1)){
+      this.curentStep = 0
+    } else { 
+      this.curentStep++
+    } 
+    
+  } //Обрабатывае событие вперед
+  
+  glrTBackward(){
+    if (this.curentStep == 0){
+      this.curentStep = this.lenghtArr-1
+    } else { 
+      this.curentStep--
+      }
+    
+  } //Обрабатывае событие назад
+
+  //------------------------------------------------------------------------Counter
+
+  glrTCheckLength(){
+    document.querySelector(`${this.target} .glrT__counter`).innerHTML ='' //очистка содержимого
+    if(this.lenghtArr <= 5){
+      for(let i=0; i < this.lenghtArr; i++){
+        document.querySelector(`${this.target} .glrT__counter`).innerHTML +=`<div class="gallery__dots"></div>`
+  
+      }
+    } else {
+        this.glrTCounter()
+      }
+  
+    } // Проверяет длинну массива изображений, если меньше 5 отрисовывает точки, если больше цифровой счетчик
+  
+  glrTCheckUpdate(){
+      if(this.lenghtArr <= 5){
+        this.glrTDotsUpdate()
+      } else {
+        this.glrTCounter()
+    }
+  }  //Проверяет длинну массива изображений в зависимости от нее вызывает апдейт Счетчика или Дотера
+  
+  glrTDotsUpdate() {
+      // console.log('DotsUpdate')
+      const allDots = document.querySelectorAll(`${this.target} .gallery__dots`)
+      allDots.forEach((dot)=>{
+        dot.style.background = "grey"
+        dot.style.transform = "scale(1)"
+      
+      }) // Сбрасывает стили у всех точек
+      
+      allDots[this.curentStep].style.background = "red"
+      allDots[this.curentStep].style.transform = "scale(1.5)"
+    } // Берёт все точки и обновляет цвет и размер в соответствии с текущим номером изображения
+
+  glrTCounter(){
+    document.querySelector(`${this.target} .glrT__counter`).innerHTML =`<div class="glrT__counterContent"> ${this.curentStep+1}/${this.lenghtArr} </div>`
+  } //Обрабатывае значение счетчика
+
+  glrTDescUpdate(){
+    document.querySelector(`${this.target} .glrT__desc`).innerHTML =`<div class="glrT__descText"> ${this.iArray[this.curentStep].desc} </div>`
+  } //Обновляет значение Description
+
+
+
+}
+
 const a = new TouchGallery('.glwrap', glrImg);
 const b = new TouchGallery('.glwrap2', glrImg,350,200);
 const c = new TouchGallery('.glwrap3', glrImg,150,150);
+const d = new TouchGalleryScale('.glwrap4', glrImg);
 
-
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------//
 //---------------------------------------------------------TestInfo
 let touchStart = false
 let isTouched = false
@@ -538,7 +942,6 @@ function setTouchState(value){
     touchStart = false;
    
   }
-
 }
 
 //------------------------------old
