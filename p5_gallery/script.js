036ebad35468ee713717b70ba19fa233fd1c3392
Artur <vashpartner@gmail.com>
2021-07-24 21:57:41 +0300

    Удаление первой версии скриптов

--- p5_gallery/script.js
+++ p5_gallery/script.js
@@ -7,92 +7,7 @@ const glrImg =[
     {link:'./img/StudioLightTest_21.jpg', desc:"Image 4 Red sphere" }
     
   ]
-  let curentStep = 0
-  const glr = document.querySelector('.gallery__item')
-  const counter = document.querySelector('.gallery__counter')
-  glr.addEventListener('click', glrAction)
-  glrImgRender()
-  // glrCounter()
-  glrCheckLength()
-  glrCheckUpdate()
-  
-  
-  function glrAction (e){
-    // console.log(e.target)
-    // console.log(e.target.classList.value)
-    // console.log(e)
-    if (e.target.classList.contains('item__right')){
-      glrForward()
-    } else if (e.target.classList.contains('item__left')){
-      glrBackward()
-    }
-  }  // Обрабатывает события, вызывает обновление кадра в зависимости от направления
-  
-  function glrForward(){
-    if (curentStep >= (glrImg.length-1)){
-      curentStep = 0
-    } else { 
-      curentStep++
-    } 
-    
-    glrImgRender()
-    // glrCounter()
-    glrCheckUpdate()
-  } //Обрабатывае событие вперед
-  
-  function glrBackward(){
-    if (curentStep == 0){
-      curentStep = glrImg.length-1
-    } else { 
-      curentStep--
-      }
-    
-    glrImgRender()
-    // glrCounter()
-    glrCheckUpdate()
-  } //Обрабатывае событие назад
-  
-  function glrImgRender(){
-    glr.style.background = `url('${glrImg[curentStep].link}') center/cover no-repeat`
-  } //Обновляет изображение
-  
-  function glrCounter(){
-    counter.innerHTML =`<div class="gallery__style"> ${curentStep+1} из ${glrImg.length} </div>`
-  } //Обрабатывае значение счетчика
-  
-  function glrCheckLength(){
-    if(glrImg.length <= 5){
-      for(let i=0; i < glrImg.length; i++){
-        counter.innerHTML +=`<div class="gallery__dots"></div>`
-  
-      }
-    } else {
-        glrCounter()
-      }
-  
-    } // Проверяет длинну массива изображений, если меньше 5 отрисовывает точки, если больше цифровой счетчик
-  
-    function glrCheckUpdate(){
-      if(glrImg.length <= 5){
-        glrDotsUpdate()
-      } else {
-        glrCounter()
-      }
-    }  //Проверяет длинну массива изображений в зависимости от нее вызывает апдейт Счетчика или Дотера
-  
-    function glrDotsUpdate() {
-      console.log('DotsUpdate')
-      const allDots = counter.querySelectorAll('.gallery__dots')
-      allDots.forEach((dot)=>{
-        dot.style.background = "grey"
-        dot.style.transform = "scale(1)"
-      
-      }) // Сбрасывает стили у всех точек
-      
-      allDots[curentStep].style.background = "red"
-      allDots[curentStep].style.transform = "scale(1.5)"
-    } // Берёт все точки и обновляет цвет и размер в соответствии с текущим номером изображения
-
+ 
 //------------------------------------------------------------------------------------------------------------------------------//
 
 
@@ -583,7 +498,7 @@ class TouchGalleryScale {
         this.moveAllBoxes(0,0)
         document.querySelector(`${this.target} .glrT__imageContent`).style.overflow = ""
         document.querySelectorAll(`${this.target} .glrT__imageItem`)[0].style.opacity = '0'
-       document.querySelectorAll(`${this.target} .glrT__imageItem`)[2].style.opacity = '0'
+        document.querySelectorAll(`${this.target} .glrT__imageItem`)[2].style.opacity = '0'
         let a = e.touches[0].clientX - e.touches[1].clientX
         let b = e.touches[0].clientY - e.touches[1].clientY
         startVectorDistance = rem(Math.sqrt(a*a+b*b))
@@ -816,6 +731,8 @@ class TouchGalleryScale {
 
 
 }
+
+//----------------------------------------------------------------------------Main Script
 
 const a = new TouchGallery('.glwrap', glrImg);
 const b = new TouchGallery('.glwrap2', glrImg,350,200);
